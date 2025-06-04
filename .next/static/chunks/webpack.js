52fc7dbc779164d698516cdb2471e59d4a40727b
devikasu <devikasubhashclg@gmail.com>
2025-06-05 05:02:49 +0530

    deleted index

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c7fee74a26d283c2"; }
+/******/ 		__webpack_require__.h = function() { return "69f50d05d73909ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
